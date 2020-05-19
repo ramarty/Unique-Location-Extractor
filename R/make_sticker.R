8cdb2eb9ac24c623651a669fd16764f7bbc9f77d
Rob Marty <ramarty@email.wm.edu>
2020-05-18 20:50:12 -0400

    img width

--- R/make_sticker.R
+++ R/make_sticker.R
@@ -20,7 +20,7 @@ areas_nairobi@data <- areas_nairobi@data %>%
 
 #### Subset
 area_sub <- areas_nairobi[1:40,]
-area_sub_buff <- gBuffer(area_sub, width=1.1/111.12, byid=F)
+area_sub_buff <- gBuffer(area_sub, width=1/111.12, byid=F)
 area_sub_buff$id <- 1
 roads_sub <- roads_nairobi %>% crop(area_sub)
 landmarks_sub <- landmark_gazetteer_orig %>% crop(area_sub)
@@ -39,22 +39,22 @@ p <- ggplot() +
 the road next to landmark near the
 occured at intersection just before
 the road next to landmark near the
-intersection just before",size=3,alpha=0.2) + 
+intersection just before",size=3,alpha=0.4) + 
   geom_polygon(data=area_sub_buff, aes(x=long, y=lat, group=group),
                fill="floralwhite", color="floralwhite", size=0.2) + 
   geom_polygon(data=area_sub, aes(x=long, y=lat, group=group),
-               fill="floralwhite", color="black", size=0.15) + 
+               fill="floralwhite", color="black", size=0.18) + 
   geom_path(data=roads_sub, aes(x=long, y=lat, group=group),
             color="gray50", size=0.05) + 
   geom_point(data=landmarks_sub, aes(x=lon, y=lat),
              color="red", size=.0025) +
   geom_point(data=landmarks_sub[10,], aes(x=lon, y=lat),
-            color="black",  fill="green", size=1.5, pch=21) +
+            color="black",  fill="green", size=2, pch=21) +
   xlim(xmin, xmax) +
   theme_void() +
   theme_transparent() 
 
-sticker(p, package="ULeX", p_color = "black", s_width=1.8, s_height=1.3,
+sticker(p, package="ULeX", p_color = "black", s_width=2, s_height=1.3,
         s_x = 1,
         p_y=1.5,
-        h_fill="floralwhite", h_color="black", filename="~/Desktop/ulex_sticker.png")
+        h_fill="floralwhite", h_color="black", filename="~/Documents/Github/Unique-Location-Extractor/man/figures/logo.png")
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Unique-Location-Extractor <img src="man/figures/logo.png" align="right" width="170" />
+# Unique Location Extractor <img src="man/figures/logo.png" align="right" width="170" />
 
 Extracts unique locations from text. Initially developed to geocode crash locations from @Ma3Route.
